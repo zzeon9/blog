f409bd1261ab5d4a53b43952ff62924fe9eaf1fc
paullabkorea <paullabkorea@gmail.com>
2024-05-09 11:16:20 +0900

    ipynb 출력 결과값 출력하도록 변경

--- style/convertIpynbToHtml.js
+++ style/convertIpynbToHtml.js
@@ -27,19 +27,24 @@ function convertIpynvToHtml(fileContent) {
             if (cell.outputs && cell.outputs.length > 0) {
                 cell.outputs.forEach(output => {
                     // output.data가 존재하는지 확인
-                    if (output.data) {
-                        if (output.output_type === 'execute_result' || output.output_type === 'display_data' || output.output_type === 'stream') {
-                            if (output.data['text/html']) {
-                                // <map object>가 들어오는 경우가 있어서 이스케이프 처리하지 않음
-                                htmlContent += output.data['text/html'].join('');
-                            } else if (output.data['text/plain']) {
-                                // pandas의 DataFrame이 들어오는 경우가 있어서 이스케이프 처리하지 않음
-                                htmlContent += `<pre>${escapeHtml(output.data['text/plain'].join(''))}</pre>`;
-                            } else if (output.data['image/png']) {
-                                htmlContent += `<img src="data:image/png;base64,${output.data['image/png'].join('')}" alt="output image" />`;
-                            } else if (output.data['image/jpeg']) {
-                                htmlContent += `<img src="data:image/jpeg;base64,${output.data['image/jpeg'].join('')}" alt="output image" />`;
+                    if (output.data || output.text) {
+                        if (output.data) {
+                            if (output.output_type === 'execute_result' || output.output_type === 'display_data' || output.output_type === 'stream') {
+                                if (output.data['text/html']) {
+                                    // <map object>가 들어오는 경우가 있어서 이스케이프 처리하지 않음
+                                    htmlContent += output.data['text/html'].join('');
+                                } else if (output.data['text/plain']) {
+                                    // pandas의 DataFrame이 들어오는 경우가 있어서 이스케이프 처리하지 않음
+                                    htmlContent += `<pre>${escapeHtml(output.data['text/plain'].join(''))}</pre>`;
+                                } else if (output.data['image/png']) {
+                                    htmlContent += `<img src="data:image/png;base64,${output.data['image/png'].join('')}" alt="output image" />`;
+                                } else if (output.data['image/jpeg']) {
+                                    htmlContent += `<img src="data:image/jpeg;base64,${output.data['image/jpeg'].join('')}" alt="output image" />`;
+                                }
                             }
+                        }
+                        if (output.text) {
+                            htmlContent += `<pre>${escapeHtml(output.text.join(''))}</pre>`;
                         }
                     } else if (output.output_type === 'error') {
                         htmlContent += `<pre class="error">${escapeHtml(output.traceback.join(''))}</pre>`;
